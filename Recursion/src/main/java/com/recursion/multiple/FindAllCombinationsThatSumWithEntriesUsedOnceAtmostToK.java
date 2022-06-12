ff425f331f024709da4fba0d93630baa065983fd
aditya <adityapandey1207@gmail.com>
2022-06-13 02:52:14 +0530

    Solved subset sums , combos sum to K with no restricitons and restriction of each entry at most picked once

--- Recursion/src/main/java/com/recursion/multiple/FindAllCombinationsThatSumWithEntriesUsedOnceAtmostToK.java
+++ Recursion/src/main/java/com/recursion/multiple/FindAllCombinationsThatSumWithEntriesUsedOnceAtmostToK.java
@@ -3,12 +3,8 @@ package com.recursion.multiple;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Set;
 
 //assumption : all positive numbers in input array
 //given : array has unique elements
@@ -59,7 +55,7 @@ public class FindAllCombinationsThatSumWithEntriesUsedOnceAtmostToK {
 
   }
 
-  public void findAllCombinationsThatSumToK4(long[] input, long k, int index,
+  public void findAllCombinationsThatSumToKWithEachEntryUsedAtmostOnce(long[] input, long k, int index,
       LinkedList<Long> thisCombination, Collection<LinkedList<Long>> allCombinations) {
 
     if (k == 0) {
@@ -74,7 +70,7 @@ public class FindAllCombinationsThatSumWithEntriesUsedOnceAtmostToK {
               break;
             else {
               thisCombination.add(input[i]);
-              findAllCombinationsThatSumToK4(input , k - input[i] , i+1 , thisCombination , allCombinations);
+              findAllCombinationsThatSumToKWithEachEntryUsedAtmostOnce(input , k - input[i] , i+1 , thisCombination , allCombinations);
               thisCombination.removeLast();
             }
        }
@@ -129,7 +125,7 @@ public class FindAllCombinationsThatSumWithEntriesUsedOnceAtmostToK {
     long[] input1 = {6, 2, 4, 3, 5, 1};
     Arrays.sort(input1);
 
-    findAllCombinationsThatSumToK.findAllCombinationsThatSumToK4(input1, 10, 0, new LinkedList<>(),
+    findAllCombinationsThatSumToK.findAllCombinationsThatSumToKWithEachEntryUsedAtmostOnce(input1, 10, 0, new LinkedList<>(),
         allCombinationsSet);
 
     System.out.println(allCombinationsSet);
@@ -138,7 +134,7 @@ public class FindAllCombinationsThatSumWithEntriesUsedOnceAtmostToK {
     long[] input2 = {3, 2, 5};
     Arrays.sort(input2);
     allCombinationsSet = new ArrayList<>();
-    findAllCombinationsThatSumToK.findAllCombinationsThatSumToK4(input2, 8, 0, new LinkedList<>(),
+    findAllCombinationsThatSumToK.findAllCombinationsThatSumToKWithEachEntryUsedAtmostOnce(input2, 8, 0, new LinkedList<>(),
         allCombinationsSet);
     System.out.println(allCombinationsSet);
 
@@ -146,7 +142,7 @@ public class FindAllCombinationsThatSumWithEntriesUsedOnceAtmostToK {
     long[] input3 = {6, 7, 3, 2};
     Arrays.sort(input3);
     allCombinationsSet = new ArrayList<>();
-    findAllCombinationsThatSumToK.findAllCombinationsThatSumToK4(input3, 7, 0, new LinkedList<>(),
+    findAllCombinationsThatSumToK.findAllCombinationsThatSumToKWithEachEntryUsedAtmostOnce(input3, 7, 0, new LinkedList<>(),
         allCombinationsSet);
     System.out.println(allCombinationsSet);
 
@@ -154,7 +150,7 @@ public class FindAllCombinationsThatSumWithEntriesUsedOnceAtmostToK {
     long[] input4 = {6, 7, 3, 2, 2, 6, 3};
     Arrays.sort(input4);
     allCombinationsSet = new ArrayList<>();
-    findAllCombinationsThatSumToK.findAllCombinationsThatSumToK4(input4, 7, 0, new LinkedList<>(),
+    findAllCombinationsThatSumToK.findAllCombinationsThatSumToKWithEachEntryUsedAtmostOnce(input4, 7, 0, new LinkedList<>(),
         allCombinationsSet);
     System.out.println(allCombinationsSet);
 
@@ -162,7 +158,7 @@ public class FindAllCombinationsThatSumWithEntriesUsedOnceAtmostToK {
     long[] input5 = {6, 7, 3, 2, 2, 6, 3};
     Arrays.sort(input5);
     allCombinationsSet = new ArrayList<>();
-    findAllCombinationsThatSumToK.findAllCombinationsThatSumToK4(input5, 29, 0, new LinkedList<>(),
+    findAllCombinationsThatSumToK.findAllCombinationsThatSumToKWithEachEntryUsedAtmostOnce(input5, 29, 0, new LinkedList<>(),
         allCombinationsSet);
     System.out.println(allCombinationsSet);
 
@@ -170,7 +166,7 @@ public class FindAllCombinationsThatSumWithEntriesUsedOnceAtmostToK {
     long[] input6 = {6, 7, 3, 2, 2, 6, 3};
     Arrays.sort(input6);
     allCombinationsSet = new ArrayList<>();
-    findAllCombinationsThatSumToK.findAllCombinationsThatSumToK4(input6, 10, 0, new LinkedList<>(),
+    findAllCombinationsThatSumToK.findAllCombinationsThatSumToKWithEachEntryUsedAtmostOnce(input6, 10, 0, new LinkedList<>(),
         allCombinationsSet);
     System.out.println(allCombinationsSet);
 
@@ -178,7 +174,7 @@ public class FindAllCombinationsThatSumWithEntriesUsedOnceAtmostToK {
     long[] input7 = { 7, 3,  6, 3};
     Arrays.sort(input7);
     allCombinationsSet = new ArrayList<>();
-    findAllCombinationsThatSumToK.findAllCombinationsThatSumToK4(input7, 10, 0, new LinkedList<>(),
+    findAllCombinationsThatSumToK.findAllCombinationsThatSumToKWithEachEntryUsedAtmostOnce(input7, 10, 0, new LinkedList<>(),
         allCombinationsSet);
     System.out.println(allCombinationsSet);
 
